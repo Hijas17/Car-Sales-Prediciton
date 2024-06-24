2b50a63d311242b8032de8c29383e7fc4cee8c0f
Hijas17 <hijassalam111@gmail.com>
2024-06-24 17:29:39 +0400

    model feature engineering changes

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 import pandas as pd
-from mangum import Mangum
 from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize the FastAPI app
@@ -22,13 +21,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-handler = Mangum(app)
-
 # Load the serialized model and scalers
 model = joblib.load('xgb_model.pkl')
 scaler_X = joblib.load('scaler_X.pkl')
 scaler_y = joblib.load('scaler_y.pkl')
 label_encoders = joblib.load('label_encoders.pkl')  # Dictionary of label encoders for categorical features
+target_encodings = joblib.load('target_encodings.pkl')
 
 # Define the input data model
 class CarDetails(BaseModel):
@@ -57,10 +55,19 @@ def predict(details: CarDetails):
         # Convert input data to a DataFrame
         input_data = pd.DataFrame([details.dict()])
         # Encode categorical features using the label encoders
+        
+        target_encoding_cols=['brand','model','trim']
+
+        for col in target_encoding_cols:
+            mean_target = target_encodings[col]
+            input_data[col + '_encoded'] = input_data[col].map(mean_target)    
+
+        input_data.drop(target_encoding_cols, axis=1, inplace=True)
+
         for col, le in label_encoders.items():
             input_data[col] = le.transform(input_data[col])
-        numerical_cols = ['kilometers', 'year']
-
+        
+        numerical_cols = ['kilometers', 'year','brand_encoded','model_encoded','trim_encoded']
         # Scale numerical features
         input_data[numerical_cols] = scaler_X.transform(input_data[numerical_cols])
 
