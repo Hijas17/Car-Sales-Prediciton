33ad4aa7929356af8f059890f9d5aae62ce5757c
Hijas Abdul Salam <hijas.abdulsalam@i-o-tech.net>
2024-06-28 16:50:15 +0400

    mobile screens implemented

--- car-sale-price-app/car-price-predictor/src/App.tsx
+++ car-sale-price-app/car-price-predictor/src/App.tsx
@@ -3,26 +3,56 @@ import React from "react";
 import CarDetailsForm from "./components/carDetailsForm/carDetailsForm";
 import { Header } from "./components/header";
 import VideoBackground from "./components/VideoBackground";
+import classNames from "classnames";
+import CarDetailsFormMobile from "./components/carDetailsForm/carDetailsFormMobile";
 
 const App: React.FC = () => {
   const description =
     "A state-of-the-art platform has been meticulously crafted to evaluate the resale value of cars in the UAE. Leveraging the comprehensive Dubizzle Cars dataset, the project entailed rigorous preprocessing to ensure unparalleled data quality and accuracy. At its core, the XGBoost model was employed for its superior performance and precision in handling intricate tabular data, training on pivotal car features to predict accurate pricing. This robust model was seamlessly deployed through APIs created with FastAPI and strategically hosted on AWS EC2 to ensure optimal scalability and reliability. The frontend, elegantly designed using React, features an intuitive form where users can input car details. The data is then processed through the APIs to deliver precise resale value predictions, providing an insightful and reliable valuation experience.";
   return (
-    <div className="relative z-10">
+    <div
+      className={classNames(
+        "relative z-10",
+        window.innerWidth <= 768 ? "h-fit" : ""
+      )}
+    >
       <VideoBackground />
       <div className="w-full min-h-screen flex flex-col">
         <Header />
-        <div className="flex flex-col absolute justify-center top-56 left-20">
-          <h1 className="text-[#a7b9e0] text-[40px] md:leading-tight 2xl:leading-normal  italic xl:text-[60px] w-[40%] font-extrabold top-56 left-20">
-          Discover the True Value of Your Dream Car
+        <div
+          className={classNames(
+            "flex flex-col justify-center",
+            window.innerWidth > 768
+              ? "absolute top-56 left-20"
+              : "items-center mx-auto text-center max-w-[375px] p-[20px]"
+          )}
+        >
+          <h1
+            className={classNames(
+              "text-[#a7b9e0] xs:text-[24px] lg:text-[40px] leading-tight 2xl:leading-normal italic xl:text-[60px] font-extrabold",
+              window.innerWidth > 768 ? " top-56 left-20 w-[40%]" : ""
+            )}
+          >
+            Discover the True Value of Your Dream Car
           </h1>
-          <span className="text-[#d5e1fc] xl:text-lg sm:text-sm w-[40%] italic  h-full left-20 top-14">
+          <span
+            className={classNames(
+              "text-[#d5e1fc] xl:text-lg xs:text-sm italic",
+              window.innerWidth > 768 ? "w-[40%] h-full left-20 top-14" : "mt-2"
+            )}
+          >
             {description}
           </span>
         </div>
-        <div className="flex-grow w-full flex items-center justify-end z-30">
-          <CarDetailsForm />
-        </div>
+        {window.innerWidth > 768 ? (
+          <div className="flex-grow w-full flex items-center justify-end z-30">
+            <CarDetailsForm />
+          </div>
+        ) : (
+          <div className="flex-grow w-full z-30 py-5 px-2">
+            <CarDetailsFormMobile />
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ car-sale-price-app/car-price-predictor/src/components/carDetailsForm/carDetailsFormMobile.tsx
@@ -0,0 +1,214 @@
+import React, { useState, useEffect, useRef, FormEvent, ChangeEvent } from 'react';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import Data from './brand_model_dict.json'
+import classNames from 'classnames';
+import { cylinders, doors, fueltype, seating_capacity, sellertype, spec, transmissiontype, warranty, years } from './formOptions';
+import { predict } from '../../api/predict';
+
+const CarDetailsFormMobile: React.FC = () => {
+  type CarDetails = {
+    brand: string;
+    model: string;
+    trim: string;
+    year: number;
+    kilometers: number;
+    regional_specs: string;
+    doors: string;
+    fuel_type: string;
+    seating_capacity: string;
+    transmission_type: string;
+    no_of_cylinders: string;
+    warranty: string;
+    seller_type: string;
+  };
+  
+  const [carDetails, setCarDetails] = useState<CarDetails>({
+    brand: '',
+    model: '',
+    trim: '',
+    year: 2015,
+    kilometers: 0,
+    regional_specs: '',
+    doors: '',
+    fuel_type: '',
+    seating_capacity: '',
+    transmission_type: '',
+    no_of_cylinders: '',
+    warranty: '',
+    seller_type: '',
+  });
+
+  type CarData = {
+    [brand: string]: {
+      [model: string]: string[];
+    };
+  };
+
+
+  const carData:CarData=Data;
+
+  const [predictedPrice, setPredictedPrice] = useState<number | null>(null);
+
+  const [Models, setModels] = useState<string[]>([]);
+  const [Trims, setTrims] = useState<string[]>([]);
+
+  function toTitleCase(str: string): string {
+    return str
+      .split(/(\s|-)/)  // Split by space or hyphen, but keep the delimiters
+      .map(word => {
+        if (word.match(/(\s|-)/)) {
+          // Return delimiters as is
+          return word;
+        } else {
+          // Capitalize the first character and lowercase the rest
+          return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+        }
+      })
+      .join('');  // Join the array back into a string
+  }
+  
+
+  const handleChange = (name: string, value: string) => {
+    if (name==='brand'){
+      setModels(Object.keys(carData[value] || []));
+      setTrims([]);
+      setCarDetails({
+        ...carDetails,
+        brand:toTitleCase(value),
+        model: '',
+        trim:''
+      });
+    }
+    else if (name==="model"){
+      setTrims(carData[carDetails.brand]?.[value] || []);
+      setCarDetails({
+        ...carDetails,
+        model:toTitleCase(value),
+        trim:''
+      });
+    }
+    else if (name === "year"){
+      setCarDetails({
+        ...carDetails,
+        year:+value,
+      });
+    }
+    else{
+      setCarDetails({
+        ...carDetails,
+        [name]: toTitleCase(value)
+      });
+    }
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const result = await predict(carDetails);
+      setPredictedPrice(result.predicted_price);
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
+  const handleKilometersChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCarDetails({
+      ...carDetails,
+      kilometers: +e.target.value,
+    });
+  };
+
+
+
+  const Dropdown = ({ name, placeholder, options }: { name: keyof CarDetails, placeholder: string, options: string[] }) => {
+    const [triggerWidth, setTriggerWidth] = useState<number>(0);
+    const triggerRef = useRef<HTMLButtonElement>(null);
+
+    useEffect(() => {
+      if (triggerRef.current) {
+        setTriggerWidth(triggerRef.current.offsetWidth);
+      }
+    }, [carDetails]);
+
+    return (
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger ref={triggerRef} className={classNames("mt-1 block text-sm w-full p-2 text-[#FFFFFF] overflow-hidden border border-gray-300 rounded-md h-9 bg-[#ffffff9f] text-left", !carDetails[name]?"italic":"")}>{carDetails[name] || placeholder}</DropdownMenu.Trigger>
+        <DropdownMenu.Content style={{ width: triggerWidth }} className="bg-white border border-gray-300 rounded-md p-2 overflow-scroll h-fit max-h-[40vh]">
+          {options.map(option => (
+            <DropdownMenu.Item
+              key={option}
+              onSelect={() => handleChange(name, option)}
+              className="cursor-pointer p-2 hover:bg-gray-200 rounded-md"
+            >
+              {option}
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    );
+  };
+
+  return (
+    <div className="h-full min-w-[30%] mx-auto p-4 bg-[#09213b9a] rounded-lg shadow-md" style={{ maxWidth: '400px' }}>
+      <h2 className="text-lg text-white font-semibold mb-4">Car Valuation</h2>
+      <form onSubmit={handleSubmit} className="space-y-4 h-full w-full">
+        <div className='flex flex-col'>
+          <label className="block text-sm font-medium text-white">Brand and Model</label>
+          <div className='flex flex-col'>
+            <Dropdown name="brand" placeholder="Enter brand" options={Object.keys(carData)} />
+            <Dropdown name="model" placeholder="Enter model" options={Models} />
+          </div>
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-white">Trim</label>
+          <Dropdown name="trim" placeholder="Enter trim" options={Trims} />
+        </div>
+        <div className='flex flex-col'>
+          <label className="block text-sm font-medium text-white">Year and Mileage</label>
+          <div className='flex flex-col'>
+            <Dropdown name="year" placeholder="Enter year" options={years} />
+            <input onChange={handleKilometersChange} placeholder="Enter mileage" className='mt-1 block text-sm w-full p-2 text-[#FFFFFF] border border-gray-300 rounded-md h-9 bg-[#ffffff9f] text-left placeholder:text-[#FFFFFF] placeholder:italic' />
+          </div>
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-white">Specification</label>
+          <Dropdown name="regional_specs" placeholder="Enter specification" options={spec} />
+        </div>
+        <div className='flex flex-col'>
+          <label className="block text-sm font-medium text-white">Fuel and Transmission Type</label>
+          <div className='flex flex-col'>
+            <Dropdown name="fuel_type" placeholder="Enter Fuel Type" options={fueltype} />
+            <Dropdown name="transmission_type" placeholder="Enter Transmission Type" options={transmissiontype} />
+          </div>
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-white">Cylinders</label>
+          <Dropdown name="no_of_cylinders" placeholder="Enter Cylinders" options={cylinders} />
+        </div>
+        <div className='flex flex-col'>
+          <label className="block text-sm font-medium text-white">Doors and Seating Capacity</label>
+          <div className='flex flex-col'>
+            <Dropdown name="doors" placeholder="Number of Doors" options={doors} />
+            <Dropdown name="seating_capacity" placeholder="Enter Seating Capacity" options={seating_capacity} />
+          </div>
+        </div>
+        <div className='flex flex-col'>
+          <label className="block text-sm font-medium text-white">Warranty and Seller Type</label>
+          <div className='flex flex-col'>
+            <Dropdown name="warranty" placeholder="Warranty?" options={warranty} />
+            <Dropdown name="seller_type" placeholder="Enter Seller Type" options={sellertype} />
+          </div>
+        </div>
+        <button type="submit" className="w-full bg-blue-600 text-white p-2 rounded-md">Value this car</button>
+      </form>
+      {predictedPrice !== null && (
+        <div className="mt-4 p-4 bg-[#dec4f794] text-[#ffffff] rounded-sm">
+          Predicted Price: {predictedPrice.toFixed(0) + " AED"}
+        </div>
+      )}
+    </div>
+  );
+  
+};
+
+export default CarDetailsFormMobile;
